c1d469913a5cd90d19db122167f6a4a5a7785ddd
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-08-04 20:17:35 +0200

    ♻️ refactor(styling): remove styled-components from primary text

--- components/PrimaryText/PrimaryText.tsx
+++ components/PrimaryText/PrimaryText.tsx
@@ -1,25 +1,20 @@
 import React from "react"
-import { Text } from "react-native"
-import styled from "styled-components"
+import { StyleSheet, Text } from "react-native"
 import { universalColors } from "../../shared/colors"
 import { getTheme } from "../../shared/getTheme"
-import { PrimaryTextProps, StyledTextProps } from "./PrimaryText.types"
+import { PrimaryTextProps } from "./PrimaryText.types"
 
 const PrimaryText = ({ children: text, forceColor, forceTheme }: PrimaryTextProps) => {
-    return (
-        <StyledText
-            color={getTheme(forceTheme, universalColors.SystemWhite, universalColors.SystemBlack)}
-            forceColor={forceColor}
-        >
-            {text}
-        </StyledText>
-    )
+    const color = getTheme(forceTheme, universalColors.SystemWhite, universalColors.SystemBlack)
+
+    return <Text style={[styles.text, { color: forceColor ?? color }]}>{text}</Text>
 }
 
 export default PrimaryText
 
-const StyledText = styled(Text)<StyledTextProps>`
-    font-size: 16px;
-    color: ${({ color, forceColor }) => forceColor ?? color};
-    margin: 4px;
-`
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 16,
+        margin: 4,
+    },
+})
