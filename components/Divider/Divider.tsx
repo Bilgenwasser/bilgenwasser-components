325937bfde2b68f954e6b67165687bb15264895a
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-08-04 18:38:27 +0200

    ♻️ refactor(styling): remove styled-components from divider

--- components/Divider/Divider.tsx
+++ components/Divider/Divider.tsx
@@ -1,23 +1,27 @@
 import React from "react"
-import { View } from "react-native"
-import styled from "styled-components"
+import { StyleSheet, View } from "react-native"
 import { darkColors, lightColors } from "../../shared/colors"
-import { DividerProps, StyledViewProps } from "./Divider.types"
 import { getTheme } from "../../shared/getTheme"
+import { DividerProps } from "./Divider.types"
 
 const Divider = ({ thickness, forceTheme }: DividerProps) => {
     const thinColors = getTheme(forceTheme, darkColors.SystemGray3, lightColors.SystemGray4)
     const boldColors = getTheme(forceTheme, darkColors.SystemGray2, lightColors.SystemGray3)
 
-    return <StyledView color={thickness === "bold" ? boldColors : thinColors} thickness={thickness} />
+    const dynamicStyles = {
+        backgroundColor: thickness === "bold" ? boldColors : thinColors,
+        height: thickness === "bold" ? 1.5 : 0.5,
+    }
+
+    return <View style={[styles.divider, dynamicStyles]} />
 }
 
 export default Divider
 
-const StyledView = styled(View)<StyledViewProps>`
-    width: 92%;
-    height: ${({ thickness }) => (thickness === "bold" ? "1.5px" : "0.5px")};
-    background: ${({ color }) => color};
-    margin: 3px 0;
-    align-self: center;
-`
+const styles = StyleSheet.create({
+    divider: {
+        width: "92%",
+        marginVertical: 3,
+        alignSelf: "center",
+    },
+})
