b34b0a1c79f2d4211d25ebff73934925732bbeaf
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-08-04 20:18:54 +0200

    ♻️ refactor(styling): remove styled-components from secondary button

--- components/SecondaryButton/SecondaryButton.tsx
+++ components/SecondaryButton/SecondaryButton.tsx
@@ -1,21 +1,20 @@
 import React from "react"
-import { Text, TouchableOpacity } from "react-native"
-import styled from "styled-components"
-import { StyledColorProps } from "../../shared/primitivTypes"
+import { StyleSheet, Text, TouchableOpacity } from "react-native"
 import { SecondaryButtonProps } from "./SecondaryButton.types"
 
 const SecondaryButton = ({ children: text, color, onPress }: SecondaryButtonProps) => {
     return (
         <TouchableOpacity onPress={onPress}>
-            <StyledText color={color}>{text}</StyledText>
+            <Text style={[styles.text, { color: color }]}>{text}</Text>
         </TouchableOpacity>
     )
 }
 
 export default SecondaryButton
 
-const StyledText = styled(Text)<StyledColorProps>`
-    color: ${({ color }) => color};
-    font-size: 18px;
-    padding: 5px;
-`
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 18,
+        padding: 5,
+    },
+})
